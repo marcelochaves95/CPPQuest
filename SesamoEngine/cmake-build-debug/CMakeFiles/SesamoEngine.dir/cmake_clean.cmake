bbe0618e6fa997c394879c098e51508bb1379b06
Marcelo Chaves <marcelochaves17@gmail.com>
2018-12-29 01:44:08 -0200

    Hello, World

---
+++ SesamoEngine/cmake-build-debug/CMakeFiles/SesamoEngine.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SesamoEngine.dir/main.cpp.o"
+  "SesamoEngine.pdb"
+  "SesamoEngine"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SesamoEngine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
