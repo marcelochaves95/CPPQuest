441616e7ab5866d3e599ece23014745a4e94c0f8
Marcelo Chaves <marcelochaves17@gmail.com>
2020-07-31 15:00:36 -0300

    Create OMenor class

---
+++ src/OMenor.cs
@@ -0,0 +1,24 @@
+namespace Linguagem
+{
+    public class OMenor : OComparacao, IOperador
+    {
+        #region IOperador Members
+
+        private string _cadeia = "<";
+        public override Cadeia Cadeia
+        {
+            get { return new Cadeia(_cadeia); }
+        }
+
+        #endregion
+
+        public OMenor()
+        {
+        }
+
+        public OMenor(int NumeroLinha)
+        {
+            this.Linha = NumeroLinha;
+        }
+    }
+}
