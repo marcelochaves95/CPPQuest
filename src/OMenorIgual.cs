bf33c7cf24d2244ab3f2a2a2c50387e977e944f3
Marcelo Chaves <marcelochaves17@gmail.com>
2020-07-31 15:02:09 -0300

    Create OMenorIgual class

---
+++ src/OMenorIgual.cs
@@ -0,0 +1,24 @@
+namespace Linguagem
+{
+    public class OMenorIgual : OComparacao, IOperador
+    {
+        #region IOperador Members
+
+        private string _cadeia = "<";
+        public override Cadeia Cadeia
+        {
+            get { return new Cadeia(_cadeia); }
+        }
+
+        #endregion
+
+        public OMenorIgual()
+        {
+        }
+
+        public OMenorIgual(int NumeroLinha)
+        {
+            this.Linha = NumeroLinha;
+        }
+    }
+}
