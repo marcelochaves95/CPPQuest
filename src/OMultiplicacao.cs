c8bb7200c1083b3f8cdf7ac53fae7c726dca7961
Marcelo Chaves <marcelochaves17@gmail.com>
2020-07-31 15:30:30 -0300

    Create OMultiplicacao class

---
+++ src/OMultiplicacao.cs
@@ -0,0 +1,24 @@
+namespace Linguagem
+{
+    public class OMultiplicacao : OMatematico, IOperador
+    {
+        #region IOperador Members
+
+        private string _cadeia = "*";
+        public override Cadeia Cadeia
+        {
+            get { return new Cadeia(_cadeia); }
+        }
+
+        #endregion
+
+        public OMultiplicacao()
+        {
+        }
+
+        public OMultiplicacao(int NumeroLinha)
+        {
+            this.Linha = NumeroLinha;
+        }
+    }
+}
