3aa973abfb5db3e266b851b6677b32deb128b493
Marcelo Chaves <marcelochaves17@gmail.com>
2020-07-31 14:49:11 -0300

    Create Tipos struct

---
+++ src/Tipos.cs
@@ -0,0 +1,25 @@
+namespace Linguagem
+{
+    public struct Tipos
+    {
+        public static string Dec
+        {
+            get { return "Decimal"; }
+        }
+
+        public static string Hex
+        {
+            get { return "Hexadecimal"; }
+        }
+
+        public static string Bin
+        {
+            get { return "Binario"; }
+        }
+
+        public static string Txt
+        {
+            get { return "Texto"; }
+        }
+    }
+}
