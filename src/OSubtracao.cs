26417f694fc80734139b00fb28ef61dce3e2edd1
Marcelo Chaves <marcelochaves17@gmail.com>
2020-07-31 15:25:43 -0300

    Create OSubtracao class

---
+++ src/OSubtracao.cs
@@ -0,0 +1,24 @@
+namespace Linguagem
+{
+    public class OSubtracao : OMatematico, IOperador
+    {
+        #region IOperador Members
+
+        private string _cadeia = "-";
+        public override Cadeia Cadeia
+        {
+            get { return new Cadeia(_cadeia); }
+        }
+
+        #endregion
+
+        public OSubtracao()
+        {
+        }
+
+        public OSubtracao(int NumeroLinha)
+        {
+            this.Linha = NumeroLinha;
+        }
+    }
+}
