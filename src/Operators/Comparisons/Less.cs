50269f1e322d17064090b7da806e9642daed1131
Marcelo Chaves <marcelochaves17@gmail.com>
2020-08-02 23:21:58 -0300

    Refactor LessOrEqual class

--- src/Operators/Comparisons/Less.cs
+++ src/Operators/Comparisons/Less.cs
@@ -2,7 +2,7 @@ namespace Sesamo.Operators.Comparisons
 {
     public class Less : Comparison
     {
-        private const string _chain = ">";
+        private const string _chain = "<";
         public override Chain Chain => new Chain(_chain);
 
         public Less()
--- src/Operators/Comparisons/LessOrEqual.cs
+++ src/Operators/Comparisons/LessOrEqual.cs
@@ -1,24 +1,17 @@
 namespace Sesamo.Operators.Comparisons
 {
-    public class LessOrEqual : Comparison, IOperator
+    public class LessOrEqual : Comparison
     {
-        #region IOperador Members
-
-        private string _cadeia = "<=";
-        public override Chain Chain
-        {
-            get { return new Chain(_cadeia); }
-        }
-
-        #endregion
+        private const string _chain = "<=";
+        public override Chain Chain => new Chain(_chain);
 
         public LessOrEqual()
         {
         }
 
-        public LessOrEqual(int NumeroLinha)
+        public LessOrEqual(int lineNumber)
         {
-            this.Linha = NumeroLinha;
+            Linha = lineNumber;
         }
     }
 }
