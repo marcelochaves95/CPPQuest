449c964e3a07be4bc9fda9926bd78298151665c0
Marcelo Chaves <marcelochaves17@gmail.com>
2020-08-02 23:18:23 -0300

    Refactor Bigger class

--- src/Operators/Comparisons/Bigger.cs
+++ src/Operators/Comparisons/Bigger.cs
@@ -2,23 +2,16 @@ namespace Sesamo.Operators.Comparisons
 {
     public class Bigger : Comparison, IOperator
     {
-        #region IOperador Members
-
-        private string _cadeia = ">";
-        public override Chain Chain
-        {
-            get { return new Chain(_cadeia); }
-        }
-
-        #endregion
+        private const string _chain = ">";
+        public override Chain Chain => new Chain(_chain);
 
         public Bigger()
         {
         }
 
-        public Bigger(int NumeroLinha)
+        public Bigger(int lineNumber)
         {
-            this.Linha = NumeroLinha;
+            Linha = lineNumber;
         }
     }
 }
