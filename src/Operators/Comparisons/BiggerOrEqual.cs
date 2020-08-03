46ebee401ab7b407efc39a65666b8b82dfc5e05c
Marcelo Chaves <marcelochaves17@gmail.com>
2020-08-02 23:19:08 -0300

    Refactor BiggerOrEqual class

--- src/Operators/Comparisons/BiggerOrEqual.cs
+++ src/Operators/Comparisons/BiggerOrEqual.cs
@@ -2,23 +2,16 @@ namespace Sesamo.Operators.Comparisons
 {
     public class BiggerOrEqual : Comparison, IOperator
     {
-        #region IOperador Members
-
-        private string _cadeia = ">=";
-        public override Chain Chain
-        {
-            get { return new Chain(_cadeia); }
-        }
-
-        #endregion
+        private const string _chain = ">=";
+        public override Chain Chain => new Chain(_chain);
 
         public BiggerOrEqual()
         {
         }
 
-        public BiggerOrEqual(int NumeroLinha)
+        public BiggerOrEqual(int lineNumber)
         {
-            this.Linha = NumeroLinha;
+            Linha = lineNumber;
         }
     }
 }
